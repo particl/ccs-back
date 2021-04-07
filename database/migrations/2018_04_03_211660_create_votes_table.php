0eda19f9641efa6f3661bdbe2c92a5ad92d4b4f1
Kewde <kewde@particl.io>
2021-04-07 19:25:14 +0000

    db: add voting table and add reference from projects

---
+++ database/migrations/2018_04_03_211660_create_votes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('block_height_start')->unique();
+            $table->unsignedInteger('block_height_end')->unique();
+            $table->unsignedInteger('blocks_yes');
+            $table->unsignedInteger('blocks_no');
+            $table->unsignedInteger('milestones_completed')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
